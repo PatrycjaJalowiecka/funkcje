ae103a436cb468e8cb523d68771975658ab542d4
Patrycja Jałowiecka <pat.jalowiecka@gmail.com>
2021-01-18 23:15:23 +0100

    fun_function.py

--- fun_function.py
+++ fun_function.py
@@ -7,7 +7,7 @@ fun_default(fun_items, muzyka = "tv")
 def fun_positional(items, /, muzyka = "radio", godzina = "18.00"):
     ##print(items, muzyka, godzina)
     pass
-fun_positional(fun_items, muzyka = "tv", godzina = "17.30")
+fun_positional(fun_items, muzyka = "tv", godzina = "19.30")
 
 def fun_keyword(items, *, muzyka = "radio", godzina = "18.00"):
     ##print(items, muzyka, godzina)
