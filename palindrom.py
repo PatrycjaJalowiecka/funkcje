71e3d777244147b94c81432ea0c8f631791658d8
Patrycja Jałowiecka <pat.jalowiecka@gmail.com>
2021-01-20 12:28:11 +0100

    lambda.py

--- palindrom.py
+++ palindrom.py
@@ -1,1 +1,9 @@
-palindrom.py
+def palindrom_check(word): 
+    ##ItFunction checks if reverse of a string is equal to string given as argument.
+    return word == word[::-1]
+word = "kot"
+ans = palindrom_check(word)
+if ans: 
+    print("yes")
+else: 
+    print("no")
